d5b144e1cc0f16cd57f88b8b05b0ea25f8cf799f
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-01-25 16:02:33 -0500

    add categoryController

--- backend/src/controllers/categoryController.js
+++ backend/src/controllers/categoryController.js
@@ -13,7 +13,7 @@ export const getAllCategories = async (req, res) => {
   }
 };
 
-//Función para crear categorias
+//Función para crear categoria
 export const createCategory = async (req, res) => {
   const { name } = req.body;
   try {
@@ -25,6 +25,68 @@ export const createCategory = async (req, res) => {
   } catch (error) {
     return res.status(500).json({
       message: "Error creating category",
+      error: error.message,
+    });
+  }
+};
+
+//Función para actualizar una nota
+export const updateCategory = async (req, res) => {
+  const categoryId = req.params.id;
+  const { name } = req.body;
+
+  try {
+    const category = await Category.findOne({
+      where: { id: categoryId },
+    });
+
+    if (!category) {
+      return res.status(404).json({
+        message: `Category with ID: ${categoryId} not found`,
+      });
+    }
+
+    await Category.update(
+      { name: name },
+      {
+        where: { id: categoryId },
+      },
+    );
+
+    const updatedCategory = await Category.findOne({
+      where: { id: categoryId },
+    });
+
+    return res.status(200).json({
+      updatedCategory,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error updated category",
+      error: error.message,
+    });
+  }
+};
+
+export const deleteCategory = async (req, res) => {
+  const categoryId = req.params.id;
+
+  try {
+    const category = await Category.findOne({ where: { id: categoryId } });
+
+    if (!category) {
+      return res
+        .status(404)
+        .json({ message: `Category with ID: ${categoryId} not found` });
+    }
+
+    await category.setNotes([]);
+    await Category.destroy({ where: { id: categoryId } });
+
+    return res.status(200).json({ message: "Successfully deleted category" });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error deleting category",
       error: error.message,
     });
   }
--- backend/src/routes/categoryRoutes.js
+++ backend/src/routes/categoryRoutes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import {
   createCategory,
+  deleteCategory,
   getAllCategories,
+  updateCategory,
 } from "../controllers/categoryController.js";
 
 const router = Router();
 
 router.get("/getCategories", getAllCategories);
 router.post("/createCategory", createCategory);
+router.put("/updateCategory/:id", updateCategory);
+router.delete("/deleteCategory/:id", deleteCategory);
 
 export default router;
