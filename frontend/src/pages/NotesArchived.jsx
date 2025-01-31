ca1a1573d87c4ac94c890ec56baec961ba5fa539
Edward Ojeda <edward.ojeda.es@gmail.com>
2025-01-31 15:11:02 -0500

    Add filter by categories

--- frontend/src/pages/NotesArchived.jsx
+++ frontend/src/pages/NotesArchived.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import { CardNote } from "../components/CardNote";
 import { Header } from "../components/Header";
 import { useNotes } from "../context/NotesContext";
 
 export function NotesArchived() {
   const { notes } = useNotes();
+  const [selectedCategory, setSelectedCategory] = useState("all");
 
   //Filtrar las notas Archivadas
-  const archivedNotes = notes.filter((note) => note.state === "note_archived");
+  const archivedNotes = notes
+    .filter((note) => note.state === "note_archived")
+    .filter((note) =>
+      selectedCategory === "all"
+        ? true
+        : note.categories.some(
+            (category) => category.name === selectedCategory,
+          ),
+    );
 
   return (
     <>
-      <Header title={"Notas Archivadas"} />
+      <Header
+        title={"Notas Archivadas"}
+        onCategoryChange={setSelectedCategory}
+      />
       <main className="w-full md:w-8/12 mx-auto grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 md:p-0 p-4 gap-3">
         {archivedNotes.map((note) => (
           <CardNote key={note.id} note={note} />
